a018b797536d4bd23a2739d8101f3880c8f82f7f
saidureg <saidureg@gmail.com>
2023-10-08 12:04:41 +0600

    fetch data and show services card in the home page

--- src/components/Services/Services.jsx
+++
@@ -1,9 +0,0 @@
-const Services = () => {
-  return (
-    <div>
-      <h3>Services</h3>
-    </div>
-  );
-};
-
-export default Services;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,25 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Header/Banner";
 import Navbar from "../../components/Header/Navbar";
 import Footer from "../Footer/Footer";
+import Services from "../Services/Services";
 
 const Home = () => {
+  const services = useLoaderData();
+  console.log(services);
   return (
     <div>
       <Navbar></Navbar>
       <Banner />
-      {/* services */}
-      <div>
-        <h3>services</h3>
+      <div className="max-w-7xl mx-auto my-16">
+        <h3 className="text-center text-5xl mb-5">Our Services</h3>
+        <div className="grid lg:grid-cols-3 gap-4">
+          {services.map((service, idx) => (
+            <Services key={idx} service={service}></Services>
+          ))}
+        </div>
       </div>
+
       <Footer />
     </div>
   );
